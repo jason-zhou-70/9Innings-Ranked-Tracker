8e6576bdcd7cc25328bc4c3dfb4d890dff7e3ffd
jason-zhou-70 <jasonzhou123@hotmail.com>
2024-09-03 13:16:06 -0700

    trying pytesseract vs tesserocr

--- scanner.py
+++ scanner.py
@@ -1,5 +1,23 @@
+from PIL import Image
+from pytesseract import pytesseract
+import tesserocr
+
 def main():
-    print()
+    # path_to_tesseract = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+    # image_path = r"C:\Users\Jason\Pictures\9Innings-Ranked-Pictures\IMG_1599_2.png"
+
+    # img = Image.open(image_path)
+
+    # pytesseract.tesseract_cmd = path_to_tesseract
+    # text = pytesseract.image_to_string(img)
+    # print(text)
+
+    api = tesserocr.PyTessBaseAPI()
+    img = Image.open(r'C:\Users\Jason\Pictures\9Innings-Ranked-Pictures\IMG_1599.png')
+    api.SetImage(img)
+    text = api.GetUTF8Text()
+    print(text)
+    print("satg")
 
 if __name__ == "__main__":
     main()
